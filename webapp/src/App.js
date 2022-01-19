832a5d192544895ebd5c551ecfb16120576810b9
HyeLee <39701029+hlpark0209@users.noreply.github.com>
2022-01-19 23:14:20 +0900

    Create user definition function

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+// 사용자 정의 태그 생성, 재사용가능
+class Subject extends Component{
+  render(){
+    return (
+      <header>
+        <h1>WEB이란?</h1>
+        World Wide Web
       </header>
-    </div>
-  );
+    )
+  }
+}
+
+class TOC extends Component{
+  render(){
+    return (
+      <nav>
+        <ol>
+          <li><a href ="1.html">HTML</a></li>
+          <li><a href ="2.html">CSS</a></li>
+          <li><a href ="3.html">JAVASCRIPT</a></li>
+        </ol>
+      </nav>
+    )
+  }
+}
+
+
+class Contents extends Component{
+  render(){
+    return (
+      <article>
+        <h2> welcome</h2>
+        Hello React !
+      </article>
+    )
+  }
+}
+
+
+
+
+
+class App extends Component {
+  render(){
+    return (
+      <div className="App">
+        
+        <Subject></Subject>
+
+        <TOC></TOC>
+
+        <Contents></Contents>
+
+
+
+
+
+      </div>
+    );
+  } 
 }
 
 export default App;
