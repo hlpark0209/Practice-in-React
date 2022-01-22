cd2d42c832057a0f18f545db583ec6f9df7b58fe
HyeLee <39701029+hlpark0209@users.noreply.github.com>
2022-01-23 01:40:55 +0900

    Mapping movie items

--- exercise/src/MovieApp.js
+++ exercise/src/MovieApp.js
@@ -1,3 +1,4 @@
+import { oneOfType } from 'prop-types';
 import { useEffect, useState } from 'react';
 
 function MovieApp() {
@@ -18,7 +19,19 @@ function MovieApp() {
     return (
         <div>
             <h2>3. Movie App ({movie.length})</h2>
-            {loading ? <h4>Loading...</h4> : null}
+            {loading ? <h4>Loading...</h4> : 
+                <div>
+                    {movie.map( item => (
+                        <ul key={item.id}>
+                            <h3><a href={item.url}>{item.title_long}</a></h3>
+                            <span>Genres: {item.genres}</span>
+                            <p>{item.summary}</p>
+                            <img src={item.medium_cover_image}/ >
+                        </ul>
+                    ))}
+                </div>        
+            }
+
         </div>
     );
 }
