419626a031a28ec412d2f5bf1eae815ba492b0aa
HyeLee <39701029+hlpark0209@users.noreply.github.com>
2022-01-23 02:27:58 +0900

    Setting the prop Types in Movie.js

--- exercise/src/Movie.js
+++ exercise/src/Movie.js
@@ -1,3 +1,5 @@
+import PropTypes from 'prop-types';
+
 // Movie의 component가 해당 정보들을 parent component( MovieApp.js )로부터 받아옴
 function Movie( {title_long, genres, summary, image}){
     return (
@@ -9,6 +11,16 @@ function Movie( {title_long, genres, summary, image}){
         </ul>
     );
 }
+
+
+// prop Types설정
+Movie.propTypes = {
+    title_long: PropTypes.string.isRequired,
+    genres: PropTypes.arrayOf(PropTypes.string).isRequired,
+    summary: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+}
+
 
 export default Movie;
 
