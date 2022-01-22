336bd79ffd20503e87341b65ebc7f05297d776d8
HyeLee <39701029+hlpark0209@users.noreply.github.com>
2022-01-23 03:31:44 +0900

    Update MovieApp.js

--- exercise/src/MovieApp.js
+++ exercise/src/MovieApp.js
@@ -1,7 +1,7 @@
 // App.js 는 영화를 보여주지 않고 ( 이전에는 영화를 보여주는 페이지였음 ) 
 // rounter를 render하는 역할로 변경 ( rounter는 URL를 보고있는 component임 )
 // MovieApp => rountes => URL 주소에 따라 1. Home.js를 render
-//                                   2. Detail.js를 render
+//                                      2. Detail.js를 render
 
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import Home from "./routes/Home";
