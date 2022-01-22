3db784b7857c5cadc4bd66f568d8dea7850ff7ba
HyeLee <39701029+hlpark0209@users.noreply.github.com>
2022-01-22 21:52:18 +0900

    Create-react-app files

---
+++ react-beginner/app.js
@@ -0,0 +1,11 @@
+import { useState, useEffec } from "react";
+
+function App(){
+    return(
+        <div>
+            <input type="text" placeholder="Write your to do"  />
+        </div>
+    ); 
+}
+
+export default App;
